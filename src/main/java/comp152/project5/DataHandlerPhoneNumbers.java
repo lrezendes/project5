17ec1c5269ab19531501407f8e5c17dde86d4fff
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-22 00:06:25 -0500

    started DataHandlerPhoneNumbers for fone number api

--- src/main/java/comp152/project5/DataHandlerPhoneNumbers.java
+++ src/main/java/comp152/project5/DataHandlerPhoneNumbers.java
@@ -1,4 +1,15 @@
 package comp152.project5;
 
+import java.net.http.HttpClient;
+
 public class DataHandlerPhoneNumbers {
+    private HttpClient dataGrabber;
+    private String webLocation;
+
+    public DataHandlerPhoneNumbers(String webLocation){
+        dataGrabber=HttpClient.newHttpClient();
+        this.webLocation=webLocation;
+    }
+    
 }
+
