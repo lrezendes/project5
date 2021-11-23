ff57f368639043e8d77e8f22d3fe0ad8845518a3
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-23 15:39:58 -0500

    added PhoneNumberDataTypes[] method getPhoneData() to class DataHandlerPhoneNumbers

--- src/main/java/comp152/project5/DataHandlerPhoneNumbers.java
+++ src/main/java/comp152/project5/DataHandlerPhoneNumbers.java
@@ -1,6 +1,13 @@
 package comp152.project5;
 
+import com.google.gson.Gson;
+import javafx.scene.control.TableRow;
+
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class DataHandlerPhoneNumbers {
     private HttpClient dataGrabber;
@@ -10,6 +17,31 @@ public class DataHandlerPhoneNumbers {
         dataGrabber=HttpClient.newHttpClient();
         this.webLocation=webLocation;
     }
-    
+
+    public PhoneNumberDataTypes[] getPhoneData(){
+        var httpBuilder= HttpRequest.newBuilder();
+        var dataRequest=httpBuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response=null;                                             //var isnt used bcuz you cannot assign a null value to var
+        try {
+            response=dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException exception) {
+            System.out.println("Error in network");
+        }
+        catch (InterruptedException e) {
+            System.out.println("Error in data transfer");
+        }
+        if (response==null){
+            System.out.println("HttpResponse response is null, exiting");
+            System.exit(-1);
+        }
+        var responseBody=response.body();
+        var gsonInterpreter=new Gson();
+        var phoneData=gsonInterpreter.fromJson(responseBody, PhoneNumberDataTypes[].class);
+        return phoneData;
+    }
+
+
+    class PhoneNumberDataTypes{}
 }
 
