9cb60efae9406efcb4778caced6380fdc5941381
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-12-10 17:25:26 -0500

    finished FirstWindowApplication.java, introduced PhoneNumberApplication.java

--- src/main/java/comp152/project5/FirstWindowApplication.java
+++ src/main/java/comp152/project5/FirstWindowApplication.java
@@ -2,6 +2,7 @@ package comp152.project5;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,6 +10,16 @@ import java.io.IOException;
 public class FirstWindowApplication extends Application {
     @Override
     public void start(Stage stage)throws IOException{
-        FXMLLoader fxmlLoader=new FXMLLoader(FirstWindowApplication.class.getResource())
+        FXMLLoader fxmlLoader=new FXMLLoader(FirstWindowApplication.class.getResource("FirstWindow-view.fxml"));
+        Scene scene=null;
+        try{
+            scene=new Scene(fxmlLoader.load(),500,500);
+        }
+        catch (IOException e){
+            System.out.println("FXML file not found");
+        }
+        stage.setTitle("Project 5 :)  Sam, Lauren and Maria");
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/comp152/project5/PhoneNumberApplication.java
@@ -0,0 +1,6 @@
+package comp152.project5;
+
+import javafx.application.Application;
+
+public class PhoneNumberApplication extends Application {
+}
