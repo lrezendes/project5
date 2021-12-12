a18a9501b477b6bfb2c4c1220414757f329b7820
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-12-12 16:15:32 -0500

    added method getPhoneDataPhone to return a hashmap from the Phone API url
    changed method getPhoneDataNames from Map to Hashmap

--- src/main/java/comp152/project5/PhoneNumberDataHandler.java
+++ src/main/java/comp152/project5/PhoneNumberDataHandler.java
@@ -7,23 +7,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneNumberDataHandler {
     private HttpClient dataGrabber;
     private String webLocation;
+    private String codePhone;
+    private String prefixPhone;
+    private String codeNames;
+    private String countryNames;
 
     public PhoneNumberDataHandler(String webLocation){
         dataGrabber=HttpClient.newHttpClient();
         this.webLocation=webLocation;
     }
 
-    public Map getPhoneData(){
+    public HashMap <String, String>getPhoneDataNames(){
         var httpBuilder= HttpRequest.newBuilder();
         var dataRequest=httpBuilder.uri(URI.create(webLocation)).build();
         HttpResponse<String> response=null;                                             //var isnt used bcuz you cannot assign a null value to var
         try {
-            response=dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+            response=dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString()); //treats the bit of data returned as string
         }
         catch (IOException exception) {
             System.out.println("Error in network");
@@ -37,8 +42,31 @@ public class PhoneNumberDataHandler {
         }
         var responseBody=response.body();
         var gsonInterpreter=new Gson();
-        var phoneData=gsonInterpreter.fromJson(responseBody, Map.class);
-        return phoneData;
+        var phoneDataNames=gsonInterpreter.fromJson(responseBody, HashMap.class);
+        return phoneDataNames;
+    }
+    public HashMap<String, String> getPhoneDataPhone(){
+        var httpBuilder=HttpRequest.newBuilder();
+        var dataRequest=httpBuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String>response=null;
+        try {
+            response=dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString()); //treats the bit of data returned as string
+        }
+        catch (IOException exception) {
+            System.out.println("Error in network");
+        }
+        catch (InterruptedException e) {
+            System.out.println("Error in data transfer");
+        }
+        if (response==null){
+            System.out.println("HttpResponse response is null, exiting");
+            System.exit(-1);
+        }
+        var responceBody=response.body();
+        var gsonInterpreter=new Gson();
+        var phoneDataPhone=gsonInterpreter.fromJson(responceBody, HashMap.class);
+        return phoneDataPhone;
+
     }
 
 }
