230bad69d307820fcb166ad25828229d14fdc37f
m1barczuk <m1barczuk@student.bridgew.edu>
2021-12-12 23:50:22 -0500

    Created/Added 'GDPDataHandler' Class to the 'GDPDataHandler.java' Java Class, Created/Added 'getData.java' Class to the 'GDPDataHandler.java' Java Class, and Adjusted/Added/Removed Comments from the 'GDPDataHandler.java' Java Class. (This is the Remainder of My Code for 'GDPDataHandler.java' from the Past 3-4 Hours that I Did Not Get to Commit Until 8 Minutes Ago! Sorry Again About Rushing the Commits, But I Think It's Best If I Actually Commit Something to This Project! All of My Code from the Past Couple of Hours Is Currently on Project5, Meaning That Any Commits After This Will Be New Changes in Real Time.)

--- src/main/java/comp152/project5/GDPDataHandler.java
+++ src/main/java/comp152/project5/GDPDataHandler.java
@@ -8,9 +8,14 @@
  */
 
 // Java Package:
-package comp152.project5;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 // Public 'GDPDataHandler' Class:
 public class GDPDataHandler {
@@ -43,3 +48,38 @@ public class GDPDataHandler {
             return date;
         }
     }
+
+    public GDPDataHandler(String webLocation){
+        dataGrabber = HttpClient.newHttpClient();
+        // This web location is the location we're passing in:
+        this.webLocation = webLocation;
+    }
+
+    public GDPDataType[] getData() {
+        var httpbuilder = HttpRequest.newBuilder();
+
+        // Creates Request Object:
+        var dataRequest = httpbuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response = null;
+        try{
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException exception){
+            System.out.println("Error with the network");
+        }
+        catch (InterruptedException e){
+            System.out.println("Error completing data transfer");
+        }
+        if(response == null){
+            System.out.println("Something went very wrong, quitting program");
+            // Negative number represents error code:
+            System.exit(-1);
+        }
+        var responseBody = response.body();
+        // creating new gson object:
+        var jsonInterpreter = new Gson();
+        var GDPData = jsonInterpreter.fromJson(responseBody,GDPDataType[].class);
+        return GDPData;
+    }
+
+}
